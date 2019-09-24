622f74aba0a15fb960c14c550f7c3be42bb29575
joaodematejr <joaodematejr@gmail.com>
2019-09-24 13:30:05 -0300

    Comit inicial

---
+++ comandos.py
@@ -0,0 +1,8 @@
+""" pip3 install jupyter """
+
+""" Software utilizado no curso anaconda  """
+
+""" https://www.anaconda.com/distribution/#download-section """
+
+
+""" Comando CMD or TERMINAL jupyter notebook """
